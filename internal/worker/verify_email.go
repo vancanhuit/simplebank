7d3e1458ba88a436ff83acbb3ed50a990fa8df60
vancanhuit <vancanhuit@gmail.com>
2026-07-14 20:36:18 +0700

    feat: add River verify-email worker with crypto-secure secret code

---
+++ internal/worker/verify_email.go
@@ -0,0 +1,59 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/riverqueue/river"
+
+	store "github.com/vancanhuit/simplebank/internal/db"
+	sqlcdb "github.com/vancanhuit/simplebank/internal/db/sqlc"
+	"github.com/vancanhuit/simplebank/internal/mail"
+	"github.com/vancanhuit/simplebank/internal/util"
+)
+
+type SendVerifyEmailArgs struct {
+	Username string `json:"username"`
+}
+
+func (SendVerifyEmailArgs) Kind() string { return "send_verify_email" }
+
+type SendVerifyEmailWorker struct {
+	river.WorkerDefaults[SendVerifyEmailArgs]
+	store   store.Store
+	mailer  mail.Mailer
+	baseURL string
+}
+
+func NewSendVerifyEmailWorker(st store.Store, mailer mail.Mailer, baseURL string) *SendVerifyEmailWorker {
+	return &SendVerifyEmailWorker{store: st, mailer: mailer, baseURL: baseURL}
+}
+
+func (w *SendVerifyEmailWorker) Work(ctx context.Context, job *river.Job[SendVerifyEmailArgs]) error {
+	user, err := w.store.GetUser(ctx, job.Args.Username)
+	if err != nil {
+		return err
+	}
+
+	code, err := util.SecureToken(32)
+	if err != nil {
+		return err
+	}
+
+	ve, err := w.store.CreateVerifyEmail(ctx, sqlcdb.CreateVerifyEmailParams{
+		Username:   user.Username,
+		Email:      user.Email,
+		SecretCode: code,
+	})
+	if err != nil {
+		return err
+	}
+
+	link := fmt.Sprintf("%s/api/v1/users/verify_email?id=%s&code=%s",
+		w.baseURL, ve.ID.String(), ve.SecretCode)
+	body := fmt.Sprintf(
+		`Hello %s,<br/>Please <a href="%s">click here</a> to verify your email address.`,
+		user.FullName, link)
+
+	return w.mailer.Send(ctx, user.Email, "Welcome to SimpleBank", body)
+}
